9cd4284c33691701ff581ed3e2ad08b2738ebfa7
Geoffrey Bachelet <grosfrais@gmail.com>
2011-09-09 16:53:34 +0200

    added basic project/show action

--- src/app.php
+++ src/app.php
@@ -2,6 +2,9 @@
 
 $app = require_once __DIR__.'/bootstrap.php';
 
+/**
+ * Homepage, lists recent projects
+ */
 $app->get('/', function() use ($app) {
     $projects = $app['db']->fetchAll('SELECT * FROM project ORDER BY id DESC LIMIT 5');
 
@@ -10,6 +13,9 @@ $app->get('/', function() use ($app) {
     ));
 })->bind('homepage');
 
+/**
+ * Project creation form
+ */
 $app->get('/project/new', function() use ($app) {
     $form = $app['form.factory']->create(new Form\ProjectType());
 
@@ -18,6 +24,20 @@ $app->get('/project/new', function() use ($app) {
     ));
 })->bind('project_new');
 
+/**
+ * Project show
+ */
+$app->get('/project/{id}', function($id) use ($app) {
+    $project = $app['db']->fetchAssoc('SELECT * FROM project WHERE id = ?', array($id));
+
+    return $app['twig']->render('Project/show.html.twig', array(
+       'project' => $project,
+    ));
+})->bind('project_show');
+
+/**
+ * Project creation
+ */
 $app->post('/project', function() use ($app) {
     $project = $app['request']->get('project');
     $form    = $app['form.factory']->create(new Form\ProjectType());
