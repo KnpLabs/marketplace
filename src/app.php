4f1ddba90df7c9b1e7b5e71d2b7ac270909020fd
Romain Pouclet <me@palleas.com>
2011-09-21 14:24:38 +0200

    Added comments count on homepage

--- src/app.php
+++ src/app.php
@@ -6,7 +6,7 @@ $app = require_once __DIR__.'/bootstrap.php';
  * Homepage, lists recent projects
  */
 $app->get('/', function() use ($app) {
-    $projects = $app['db']->fetchAll('SELECT * FROM project ORDER BY id DESC');
+    $projects = $app['db']->fetchAll('SELECT p.*, COUNT(c.id) as comments FROM project p LEFT JOIN comment c ON c.project_id = p.id GROUP BY p.id ORDER BY id DESC');
 
     return $app['twig']->render('homepage.html.twig', array(
         'projects' => $projects,
