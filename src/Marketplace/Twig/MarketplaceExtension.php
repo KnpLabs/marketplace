e05bf8416a58af7388f8bb1f8823ec0ac216c533
Geoffrey Bachelet <grosfrais@gmail.com>
2011-10-14 18:12:22 +0200

    added gravatar support

--- src/Marketplace/Twig/MarketplaceExtension.php
+++ src/Marketplace/Twig/MarketplaceExtension.php
@@ -14,6 +14,18 @@ class MarketplaceExtension extends \Twig_Extension
         return array('username' => new \Twig_Filter_Method($this, 'username', array('is_safe' => array('html'))));
     }
 
+    public function getFunctions()
+    {
+        return array('gravatar' => new \Twig_Function_Method($this, 'gravatar'));
+    }
+
+    public function gravatar($email, $size = 50, $default = 'mm')
+    {
+        $hash = md5(strtolower($email));
+
+        return sprintf('http://www.gravatar.com/avatar/'.$hash.'?s='.$size.'&d='.$default);
+    }
+
     public function username($string)
     {
         $firstname = substr($string, 0, strpos($string, '.'));
