021f25a537b85213e3f4f059eaef0db53ef0836d
Romain Pouclet <me@palleas.com>
2011-11-04 17:31:12 +0100

    Add the very first scenario of the very first behat feature.

--- src/Marketplace/features/bootstrap/FeatureContext.php
+++ src/Marketplace/features/bootstrap/FeatureContext.php
@@ -7,19 +7,32 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
+use Behat\Mink\Behat\Context\MinkContext;
+
+require_once __DIR__.'/../../../../app/bootstrap.php';
+
 /**
  * Features context.
  */
-class FeatureContext extends BehatContext
+class FeatureContext extends MinkContext
 {
+
     /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param   array   $parameters     context parameters (set them up through behat.yml)
+     * @var Silex\Application
      */
-    public function __construct(array $parameters)
+    private $app;
+
+    public function __construct(array $parameters = array())
     {
+        parent::__construct(array_merge(array(
+            'base_url' => 'http://ideamarketplace.local'
+        ), $parameters));
+
+        // We remove the cache file 
+        // Todo : find a more dynamic way
+        if (file_exists($dbfile = __DIR__.'/../../../../cache/app.db')) {
+            unlink($dbfile);
+        }
     }
 
 }
--- src/Marketplace/features/project-new.feature
+++ src/Marketplace/features/project-new.feature
@@ -3,4 +3,10 @@ Feature: Add a project
   I should be able to create a project
 
   Scenario: add a project
-    
+    Given I am on "/index_test.php"
+    When I follow "New project"
+    And I fill in "Name" with "Dominate world"
+    And I select "random" from "Category"
+    And I fill in "Description" with "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb."
+    And I press "Create project"
+    Then I should see "Dominate world"
