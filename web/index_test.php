9bc22244da49081b92231916d0dda0e50fa46b47
Romain Pouclet <me@palleas.com>
2011-11-04 16:11:14 +0100

    Bypass auth w/ a test endpoint

---
+++ web/index_test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This is the test environnement endpoint (used running behat features for example)
+ */
+$app = require_once __DIR__.'/../app/marketplace.php';
+
+// The application redirects to Google auth page if
+// there is not a username in session, so we had a fake one
+$app['session']->set('username', 'edgar@knplabs.com');
+
+// The application use a Closure to check if the username, so 
+// we override this service and use a closure which always
+// returns true 
+$app['auth'] = $app->share(function() use ($app) {
+    return function($username) use ($app) {
+      return true;
+    };
+});
+
+$app->run();
