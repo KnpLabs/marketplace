5820b13b02462eaf2021820efd1de590f619a6e8
Romain Pouclet <me@palleas.com>
2011-11-04 17:25:07 +0100

    Change fake user identity

--- web/index_test.php
+++ web/index_test.php
@@ -6,7 +6,7 @@ $app = require_once __DIR__.'/../app/marketplace.php';
 
 // The application redirects to Google auth page if
 // there is not a username in session, so we had a fake one
-$app['session']->set('username', 'edgar@knplabs.com');
+$app['session']->set('username', 'edgar.thecat@knplabs.com');
 
 // The application use a Closure to check if the username, so 
 // we override this service and use a closure which always
