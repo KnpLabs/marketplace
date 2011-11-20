a74f8b0986706b9cd34572cc7803f37207a9799c
Geoffrey Bachelet <grosfrais@gmail.com>
2011-11-20 20:47:36 +0100

    fixed preview tabs by adding explicit active tab

--- web/js/marketplace.js
+++ web/js/marketplace.js
@@ -1,28 +1,24 @@
 jQuery(function($) {
-	var 
-		// URL we send the ajax request to 
-		url = $(".show_preview_link").data("url"),
-		// Contain transformed HTML
-		preview_container = $("#preview"),
-		// Textara the comment is typed into
-		field = $(".comment_field");
+	$(document).ready(function() {
+		var 
+			// URL we send the ajax request to 
+			url = $(".show_preview_link").data("url"),
+			// Contain transformed HTML
+			preview_container = $("#preview"),
+			// Textara the comment is typed into
+			field = $(".comment_field");
 
-	/** 
-	 * Handle tab selection change
-	 * (and trigger preview if user selected the "preview" tab)
-	 */
-	function onTabChange(event) {
-		var target = $(event.target);
-		
-		// If we clicked on the preview link		
-		if ($(event.target).hasClass("show_preview_link")) {
-			preview_container.text("Loading preview...");
+		$(".preview_tabs").show().tabs().bind("change", function(event) {
+			var target = $(event.target);
+			
+			// If we clicked on the preview link		
+			if ($(event.target).hasClass("show_preview_link")) {
+				preview_container.text("Loading preview...");
 
-			$.post(url, { "markdown_content" : field.val() }, function (response, textStatus, connection) {
-				preview_container.html(response);
-			}, "html");
-		}		
-	}
-
-	$(".preview_tabs").show().tabs().bind("change", onTabChange);
+				$.post(url, { "markdown_content" : field.val() }, function (response, textStatus, connection) {
+					preview_container.html(response);
+				}, "html");
+			}		
+		});
+	});
 });
